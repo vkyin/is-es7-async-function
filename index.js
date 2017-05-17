118158a373b6b3513e6c9c9a15d95b52eee81331
vkyin <86160576@qq.com>
2017-05-18 00:54:30 +0800

    update and add travis

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
+let toStrFn = Object.prototype.toString;
 /**
  * here define an async function
  * it will throw a SyntaxError while your javascript runtime is not support AsyncFunction featrue
  */
-let asyncFunction = async function () {};
+let asyncFunction = async function () { };
+
+let prototypeOfAsyncFunction = Object.getPrototypeOf(asyncFunction);
 
 /**
- * 
+ * normally, it will return 'AsyncFunction'
  */
-let protoOfAsyncFunction = Object.getPrototypeOf(asyncFunction);
+let asyncFunctionName = asyncFunction.constructor.name;
 
-let toStrFn = Object.prototype.toString;
-
+/**
+ * normally, it will return '[object AsyncFunction]'
+ */
 let protoStr = toStrFn.call(asyncFunction);
 
-let isAsyncFunction = function (obj){
-    return toStrFn.call(obj) === protoStr;
+let isAsyncFunction = function (obj) {
+    return obj.constructor
+        && obj.constructor.name === asyncFunctionName
+        && toStrFn.call(obj) === protoStr
+        && Object.getPrototypeOf(obj) === prototypeOfAsyncFunction;
 }
 
 module.exports = isAsyncFunction;
